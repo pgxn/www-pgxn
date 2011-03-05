82fb65c9c2d2099fa6d59f34e8313cba4e0e3e71
David E. Wheeler <david@justatheory.com>
2011-03-05 15:03:24 -0800

    Start implementation.
    Fetch templates using HTTP::Tiny or the local file system (emulating the
    interface of HTTP::Tiny).

--- lib/WWW/PGXN.pm
+++ lib/WWW/PGXN.pm
@@ -2,9 +2,103 @@ package WWW::PGXN;
 
 use 5.8.1;
 use strict;
+use HTTP::Tiny;
+use URI::Template;
+use JSON;
+use Carp;
 
 our $VERSION = '0.10';
+my @attributes = qw(url proxy);
 
+sub new {
+    my($class, %args) = @_;
+    my $self = bless {} => $class;
+    for my $key ( @attributes ) {
+        $self->$key($args{$key}) if exists $args{$key}
+    }
+    return $self;
+}
+
+sub find_distribution {
+    my ($self, $dist) = @_;
+#    my $res = $http->get()
+}
+
+sub url {
+    my $self = shift;
+    return $self->{url} unless @_;
+    ($self->{url} = shift) =~ s{/+$}{}g;
+    delete $self->{_req};
+    $self->{url};
+}
+
+sub proxy {
+    my $self = shift;
+    return $self->{proxy} unless @_;
+    $self->{proxy} = shift;
+}
+
+sub _uri_templates {
+    my $self = shift;
+    return $self->{uri_templates} ||= { do {
+        my $req = $self->_request;
+        my $url = $self->url . '/index.json';
+        my $res = $req->get($url);
+        croak "Request for $url failed: $res->{status}: $res->{reason}\n"
+            unless $res->{success};
+        my $tmpl = JSON->new->utf8->decode($res->{content});
+        map { $_ => URI::Template->new($tmpl->{$_}) } keys %{ $tmpl };
+    }};
+}
+
+sub _request {
+    my $self = shift;
+    $self->{_req} ||= $self->url =~ m{^file:} ? WWW::PGXN::FileReq->new : HTTP::Tiny->new(
+        agent => __PACKAGE__ . "/$VERSION",
+        proxy => $self->proxy,
+    );
+}
+
+package
+WWW::PGXN::FileReq;
+
+use strict;
+use URI::file;
+use File::Spec::Functions qw(catfile);
+use namespace::autoclean;
+
+sub new {
+    bless {} => shift;
+}
+
+sub get {
+    my $self = shift;
+    (my $file = shift) =~ s{^file:}{};
+    $file = catfile split m{/}, $file;
+
+    return {
+        success => 0,
+        status  => 404,
+        reason  => 'not found',
+        headers => {},
+    } unless -e $file;
+
+    open my $fh, '<:raw', $file or return {
+        success => 0,
+        status  => 500,
+        reason  => $!,
+        headers => {},
+    };
+
+    local $/;
+    return {
+        success => 1,
+        status  => 200,
+        reason  => 'OK',
+        content => <$fh> || undef,
+        headers => {},
+    };
+}
 
 1;
 __END__
@@ -25,6 +119,58 @@ WWW::PGXN - Interface to the PGXN API
 
 This module provide a simple Perl interface over the the L<PGXN
 API|http://api.pgxn.org/>.
+
+=head1 Interface
+
+=head2 Constructor
+
+=head3 C<new>
+
+  my $pgxn = WWW::PGXN->new(
+      url => 'http://api.pgxn.org/',
+  );
+
+Construct a new WWW::PGXN object. The only required attribute is C<url>. The
+supported parameters are:
+
+=over
+
+=item C<url>
+
+The base URL for the API server or mirror the client should fetch from.
+Required.
+
+=item C<proxy>
+
+URL of a proxy server to use.
+
+=back
+
+=head2 Instance Accessors
+
+=head3 C<url>
+
+  my $url = $pgxn->url;
+  $pgxn->url($url);
+
+Get or set the URL for the PGXN mirror or API server.
+
+=head3 C<proxy>
+
+  my $proxy = $pgxn->proxy;
+  $pgxn->proxy($proxy);
+
+Get or set the URL for a proxy server to use.
+
+=head2 Instance Methods
+
+=head3 C<find_distribution>
+
+  my $dist = $pgxn->find_distribution($dist_name);
+
+Finds the data for the named distribution. Returns a
+L<WWW::PGXN::Distribution> object.
+
 =head1 See Also
 
 =over
--- t/base.t
+++ t/base.t
@@ -2,7 +2,67 @@
 
 use strict;
 use warnings;
-use Test::More tests => 1;
-#use Test::More 'no_plan';
+#use Test::More tests => 1;
+use Test::More 'no_plan';
+use File::Spec::Functions qw(catfile);
 
-BEGIN { use_ok 'WWW::PGXN' or die; }
+my $CLASS;
+BEGIN {
+    $CLASS = 'WWW::PGXN';
+    use_ok $CLASS or die;
+}
+
+my $pgxn = new_ok $CLASS, [ url => 'http://api.pgxn.org/' ];
+is $pgxn->url, 'http://api.pgxn.org', 'Should have the URL';
+is $pgxn->proxy, undef, 'Should have no proxy';
+
+##############################################################################
+# Test the request object.
+isa_ok $pgxn->_request, 'HTTP::Tiny', 'The request object';
+
+# Switch to local files.
+ok $pgxn->url('file:t/mirror'), 'Switch to local mirror';
+isa_ok my $req = $pgxn->_request, 'WWW::PGXN::FileReq', 'The request object';
+
+##############################################################################
+# Test FileReq.
+ok my $res = $req->get('file:t/nonexistent.txt'),
+    'Fetch nonexisent file';
+is_deeply $res, {
+    success => 0,
+    status  => 404,
+    reason  => 'not found',
+    headers => {},
+}, 'Should have "not found" response';
+
+my $f = catfile qw(t mirror index.json);
+open my $fh, '<:raw', $f or die "Cannot open $f: $!\n";
+my $json = do {
+    local $/;
+    <$fh>;
+};
+close $fh;
+
+ok $res = $req->get('file:t/mirror/index.json'), 'Fetch index.html';
+is_deeply $res, {
+    success => 1,
+    status  => 200,
+    reason  => 'OK',
+    content => $json,
+    headers => {},
+}, 'Should have the content response';
+
+##############################################################################
+# Test the templates. Start with a bogus URL.
+$pgxn->url('file:t');
+local $@;
+eval { $pgxn->_uri_templates };
+like $@, qr{Request for file:t/index\.json failed: 404: not found},
+    'Should get exception for bad templates URL';
+
+# Now get the real thing.
+$pgxn->url('file:t/mirror');
+ok my $tmpl =  $pgxn->_uri_templates, 'Get the URI templates';
+my $data = JSON->new->utf8->decode($json);
+is_deeply $tmpl, { map { $_ => URI::Template->new($data->{$_}) } keys %{ $data } },
+    'Should have all the templates';
